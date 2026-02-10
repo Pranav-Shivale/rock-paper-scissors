f60da303f5cf04b292b0481870795bd27519baaf
Pranav-Shivale <pranavshivale69@gmail.com>
2026-02-10 18:17:58 +0530

    Create and define getComputerChoice function
    Function returns either "rock", "paper" or "scissors" value using Math.random() function.

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let randNum = Math.random();
+
+  if(randNum < 0.3) {
+    return "rock";
+  }
+  else if(randNum < 0.6) {
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+}
