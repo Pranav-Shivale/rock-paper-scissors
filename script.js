64d858e8238dfafe24604412e0f685d783894d77
Pranav-Shivale <pranavshivale69@gmail.com>
2026-02-10 19:22:09 +0530

    Create playGame function
    Calls the playRound function five times for a single round.
    Keeps track of humanScore and computerScore and decides who won the Game after five rounds.

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let randNum = Math.random();
 
-  if(randNum < 0.3) {
+  if (randNum < 0.3) {
     return "rock";
-  }
-  else if(randNum < 0.6) {
+  } else if (randNum < 0.6) {
     return "paper";
-  }
-  else {
+  } else {
     return "scissors";
   }
 }
 
 function getHumanChoice() {
   let userInput = prompt("Rock, Paper, Scissors!");
-  
+
   return userInput;
 }
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (
+      (humanChoice == "rock" && computerChoice == "scissors") ||
+      (humanChoice == "paper" && computerChoice == "rock") ||
+      (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+      humanScore += 1;
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}.`);
+    } else if (
+      (humanChoice == "scissors" && computerChoice == "rock") ||
+      (humanChoice == "rock" && computerChoice == "paper") ||
+      (humanChoice == "paper" && computerChoice == "scissors")
+    ) {
+      computerScore += 1;
+      console.log(`You Lose! ${computerChoice} beats ${humanChoice}.`);
+    } else {
+      console.log(`Tie.`);
+    }
+  }
+
+  for(let i=0; i<5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+
+  console.log(`Game Score: ${humanScore} - ${computerScore}`);
+  if(humanScore > computerScore) {
+    console.log("Game Result: YOU WIN");
+  } else if(computerScore > humanScore) {
+    console.log("Game Result: YOU LOSE");
+  } else {
+    console.log("Game Result: TIE");
+  }
+}
+
+playGame();
