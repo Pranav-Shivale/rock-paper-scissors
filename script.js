e5f81a93bb9cbdf35f7cd6008ee44911e3e91339
Pranav-Shivale <pranavshivale69@gmail.com>
2026-02-19 13:23:45 +0530

    Add event listeners to buttons

--- script.js
+++ script.js
@@ -10,30 +10,45 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let userInput = prompt("Rock, Paper, Scissors!");
-
-  return userInput;
-}
-
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
-
   if (
     (humanChoice == "rock" && computerChoice == "scissors") ||
     (humanChoice == "paper" && computerChoice == "rock") ||
     (humanChoice == "scissors" && computerChoice == "paper")
   ) {
-    humanScore += 1;
     console.log(`You Win! ${humanChoice} beats ${computerChoice}.`);
   } else if (
     (humanChoice == "scissors" && computerChoice == "rock") ||
     (humanChoice == "rock" && computerChoice == "paper") ||
     (humanChoice == "paper" && computerChoice == "scissors")
   ) {
-    computerScore += 1;
     console.log(`You Lose! ${computerChoice} beats ${humanChoice}.`);
   } else {
     console.log(`Tie.`);
   }
 }
+
+let btnRock = document.querySelector("#btn-rock");
+let btnPaper = document.querySelector("#btn-paper");
+let btnScissors = document.querySelector("#btn-scissors");
+
+btnRock.addEventListener("click", () => { 
+  console.log("Rock");
+  let humanChoice = "rock";
+  let computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
+
+btnPaper.addEventListener("click", () => {
+  console.log("Paper");
+  let humanChoice = "paper";
+  let computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
+
+btnScissors.addEventListener("click", () => {
+  console.log("Scissors");
+  let humanChoice = "scissors";
+  let computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
